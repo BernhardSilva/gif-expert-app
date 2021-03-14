efc05c91700ae6d9d7a757bb710e6b7c5e388f52
Bernhard Silva <bernhard.silva@gmail.com>
2021-03-14 09:28:22 -0300

    custom Hook  useFetchGif complete

--- src/Hooks/useFetchGifs.js
+++ src/Hooks/useFetchGifs.js
@@ -1,17 +1,20 @@
 import {useState, useEffect} from 'react';
+import {getGifs} from '../helpers/getGifs';
 
-export const useFetchGifs = () => {
+export const useFetchGifs = (category) => {
   const [state, setstate] = useState({
     data: [],
     loading: true,
   });
 
-  setTimeout(() => {
-    setstate({
-      data: [1, 2, 3, 4, 5, 6, 7],
-      loading: false,
+  useEffect(() => {
+    getGifs(category).then((imgs) => {
+      setTimeout(() => {
+        console.log(imgs);
+        setstate({data: imgs, loading: false});
+      }, 3000);
     });
-  }, 3000);
+  }, [category]);
 
   return state; // {data:[], loading:true}
 };
--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,25 +1,19 @@
 import React from 'react';
-// import {GifGridItem} from './GifGridItem';
-import {getGifs} from '../helpers/getGifs';
+import {GifGridItem} from './GifGridItem';
 import {useFetchGifs} from '../Hooks/useFetchGifs';
 
 export const GifGrid = ({category}) => {
-  //   const [images, setImages] = useState([]);
-  const {loading} = useFetchGifs();
-  console.log({loading});
-  //   useEffect(() => {
-  //     getGifs(category).then(setImages);
-  //   }, [category]);
+  const {data: images, loading} = useFetchGifs(category);
 
   return (
     <>
       <h3>{category}</h3>
-      {loading ? 'Cargando...' : 'Data cargada'}
-      {/* <div className="card-grid">
+      {loading && <p>Loading...</p>}
+      <div className="card-grid">
         {images.map((img) => (
           <GifGridItem key={img.id} {...img} />
         ))}
-      </div> */}
+      </div>
     </>
   );
 };
