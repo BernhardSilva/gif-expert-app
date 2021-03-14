f33e6373b33271723d8977b724c934351c9050e4
Bernhard Silva <bernhard.silva@gmail.com>
2021-03-14 07:13:46 -0300

    GifGridItem added and add props communication with GifGrid comp

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,28 +1,39 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {GifGridItem} from './GifGridItem';
 
-export const GifGrid = ({ category }) => {
-	const getGifs = async () => {
-		const url =
-			'https://api.giphy.com/v1/gifs/search?q=Rick+and+Morty&limit=10&api_key=6gmRs6lTMlSgBxbHxyZecMCwVpEctKOS';
-		const resp = await fetch(url);
-		const { data } = await resp.json();
+export const GifGrid = ({category}) => {
+  const [images, setImages] = useState([]);
 
-		const gifs = data.map((img) => {
-			return {
-				id: img.id,
-				title: img.title,
-				url: img.images?.downsized_medium.url,
-			};
-		});
+  useEffect(() => {
+    getGifs();
+  }, []);
 
-		console.log(gifs);
-	};
+  const getGifs = async () => {
+    const url =
+      'https://api.giphy.com/v1/gifs/search?q=Rick+and+Morty&limit=10&api_key=6gmRs6lTMlSgBxbHxyZecMCwVpEctKOS';
+    const resp = await fetch(url);
+    const {data} = await resp.json();
 
-	getGifs();
+    const gifs = data.map((img) => {
+      return {
+        id: img.id,
+        title: img.title,
+        url: img.images?.downsized_medium.url,
+      };
+    });
 
-	return (
-		<div>
-			<h3>{category}</h3>
-		</div>
-	);
+    console.log(gifs);
+    setImages(gifs);
+  };
+
+  // getGifs();
+
+  return (
+    <div>
+      <h3>{category}</h3>
+      {images.map((img) => (
+        <GifGridItem key={img.id} {...img} />
+      ))}
+    </div>
+  );
 };
