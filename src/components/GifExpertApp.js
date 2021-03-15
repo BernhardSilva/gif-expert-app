a073ffcfd4af610417d05694b7756ac78c151115
Bernhard Silva <bernhard.silva@gmail.com>
2021-03-15 00:30:12 -0300

    test giphy logo and label to use api on prod

--- src/components/GifExpertApp.js
+++ src/components/GifExpertApp.js
@@ -2,13 +2,17 @@ import React, {useState} from 'react';
 import {AddCategory} from './AddCategory';
 import {GifGrid} from './GifGrid';
 import logo from '../assets/logo.png';
+import logoGifexpert from '../assets/logo-gifexpert.png';
 
 export const GifExpertApp = () => {
   const [categories, setCategories] = useState(['Shingeki no Kyojin']);
 
   return (
     <>
+      <label className="giphy-message">Powered by Giphy</label>
       <img className="logo" src={logo} alt="logo" />
+      <img className="logo-gifexpert" src={logoGifexpert} alt="logo-gifexpert" />
+
       <AddCategory setCategories={setCategories} />
 
       <ol>
--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,7 +1,7 @@
 export const getGifs = async (category) => {
   const api_uri = 'api.giphy.com/v1/gifs/search';
   const limit = 10;
-  const api_key = 'Bq2tAb37eTmTVsKJJLXUbRqNNNxV3Xd6';
+  const api_key = 'dSyzMLEWCrMsfJwfVfMuwhY3XxWCMWPW';
   const url = `https://${api_uri}?q=${encodeURI(category)}&limit=${limit}&api_key=${encodeURI(api_key)}`;
   const resp = await fetch(url);
   const {data} = await resp.json();
