09e20106ced8aea1558915780ae150921aa3628b
Bernhard Silva <bernhard.silva@gmail.com>
2021-03-15 00:45:59 -0300

    test giphy logo and label to use api on prod2

--- src/components/GifExpertApp.js
+++ src/components/GifExpertApp.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import {AddCategory} from './AddCategory';
 import {GifGrid} from './GifGrid';
-import logo from '../assets/logo.png';
+// import logo from '../assets/logo.png';
 import logoGifexpert from '../assets/logo-gifexpert.png';
 
 export const GifExpertApp = () => {
@@ -9,8 +9,8 @@ export const GifExpertApp = () => {
 
   return (
     <>
-      <label className="giphy-message">Powered by Giphy</label>
-      <img className="logo" src={logo} alt="logo" />
+      {/* <label className="giphy-message">Powered by Giphy</label>
+      <img className="logo" src={logo} alt="logo" /> */}
       <img className="logo-gifexpert" src={logoGifexpert} alt="logo-gifexpert" />
 
       <AddCategory setCategories={setCategories} />
--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,7 +1,7 @@
 export const getGifs = async (category) => {
   const api_uri = 'api.giphy.com/v1/gifs/search';
   const limit = 10;
-  const api_key = 'dSyzMLEWCrMsfJwfVfMuwhY3XxWCMWPW';
+  const api_key = 'Lp0q3mLXNkPk3vtr9MFR9qCq8hLyBPPb';
   const url = `https://${api_uri}?q=${encodeURI(category)}&limit=${limit}&api_key=${encodeURI(api_key)}`;
   const resp = await fetch(url);
   const {data} = await resp.json();
