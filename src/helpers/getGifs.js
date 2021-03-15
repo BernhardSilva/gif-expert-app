0edfcf268a6cda9fcc0dfae8ad1274ba1930eb80
Bernhard Silva <bernhard.silva@gmail.com>
2021-03-14 23:22:53 -0300

    css features

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,8 +1,8 @@
 export const getGifs = async (category) => {
   const api_uri = 'api.giphy.com/v1/gifs/search';
   const limit = 10;
-  const api_key = '6gmRs6lTMlSgBxbHxyZecMCwVpEctKOS';
-  const url = `https://${api_uri}?q=${encodeURI(category)}&limit=${limit}&api_key=${api_key}`;
+  const api_key = 'Bq2tAb37eTmTVsKJJLXUbRqNNNxV3Xd6';
+  const url = `https://${api_uri}?q=${encodeURI(category)}&limit=${limit}&api_key=${encodeURI(api_key)}`;
   const resp = await fetch(url);
   const {data} = await resp.json();
 
